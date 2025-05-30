e7d89bca7de6e3edb9c64a999e200160537dc2a1
yato modas <evellinhilario@gmail.com>
2025-05-29 21:36:20 -0300

    Add files via upload

--- dono/menus.js
+++ dono/menus.js
@@ -1,56 +1,90 @@
-
 const menu = (donoName, botName, prefixo, sender) => { 
-return `╭─⟢ 𝙄𝙉𝙁𝙊 𝘿𝘼 𝙍𝘼𝙔𝘼
-│🤖 Nome: ${botName}
-│👤 Usuário: @${sender.split("@")[0]}
-│👑 Dono: ${donoName}
-│🛠️ Versão: 1.0.0
+return `╭─⟢ 
+│ 🤖 Nome: ${botName}
+│ 👤 Usuário: @${sender.split("@")[0]}
+│ 👑 Dono: ${donoName}
+│ ⚙️ Prefixo: [ ${prefixo} ]
+│ 🛠️ Versão: 1.5.0
 ╰─────────────⟡
 
-╭─⟢ 𝘼𝘿𝙈𝙄𝙉𝙎
-│➕ ${prefixo}add (@marque)
-│⛔ ${prefixo}banir (número)
-│⬆️ ${prefixo}promover (@marque)
-│⬇️ ${prefixo}rebaixar (@marque)
-│⚡ ${prefixo}ping
-│ℹ️ ${prefixo}infodono
-│💡 ${prefixo}sugestao
-│⭐ ${prefixo}avalie
+╭─⟢ 🛡️ *ADMINISTRATIVO*
+│ 🎀 ${prefixo}add 558899..
+│ ➥ Adiciona um novo membro
+│ 🔒 ${prefixo}banir @usuário
+│ ➥ Remove e bloqueia do grupo
+│ ⬆️ ${prefixo}promover @usuário
+│ ➥ Torna o membro um admin
+│ ⬇️ ${prefixo}rebaixar @usuário
+│ ➥ Remove permissão de admin
+│ ⚙️ ${prefixo}ping
+│ ➥ Testa a velocidade do bot
+│ 🧠 ${prefixo}infodono
+│ ➥ Informações do criador
+│ 💬 ${prefixo}sugestao [texto]
+│ ➥ Envie uma sugestão ao dono
+│ 🌟 ${prefixo}avalie [nota]
+│ ➥ Der sua avaliaçao
 ╰─────────────⟡
 
-╭─⟢ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿𝙎
-│💽 ${prefixo}play (nome)
-│📽 ${prefixo}play_video (nome)
-│📽 ${prefixo}tiktok
-│📽 ${prefixo}instagram
-│📽 ${prefixo}kwai
-│📽 ${prefixo}pin_video
+╭─⟢ ⬇️ *DOWNLOADS*
+│ 🎧 ${prefixo}play_audio (nome)
+│ ➥ Download audio do youtube
+│ 🎬 ${prefixo}play_video (nome)
+│ ➥ Download video do youtube
+│ 📁 ${prefixo}play_doc (nome)
+│ ➥ Download documento video
+│ 🎵 ${prefixo}tiktok_audio (link)
+│ ➥ Download audio do tiktok
+│ 🎥 ${prefixo}tiktok_video (link)
+│ ➥ Download video do tiktok
+│ 📸 ${prefixo}instagram (link)
+│ ➥ Download video do instagram
+│ 🎞️ ${prefixo}kwai (link)
+│ ➥ Download video do kwai
+│ 🎬 ${prefixo}pin_video (link)
+│ ➥ Download video do pinterest
 ╰─────────────⟡
 
-╭─⟢ 𝙁𝙄𝙂𝙐𝙍𝙄𝙉𝙃𝘼𝙎
-│🎲 ${prefixo}sticker
-│🎲 ${prefixo}toimg
-│🎲 ${prefixo}rename
-│🎲 ${prefixo}figu-random
-│🎲 ${prefixo}figu-raiva
-│🎲 ${prefixo}figu-desenho
-│🎲 ${prefixo}figu-flork
-│🎲 ${prefixo}figu-roblox
-│🎲 ${prefixo}figu-anime
-│🎲 ${prefixo}figu-coreana
-│🎲 ${prefixo}figu-animais
-│🎲 ${prefixo}figu-engracada
-│🎲 ${prefixo}figu-bebe
+╭─⟢ 🖼️ *FIGURINHAS*
+│ 🎲 ${prefixo}sticker (menc-foto)
+│ ➥ Converter imagem em sticker
+│ 🖼️ ${prefixo}toimg (menc-sticker)
+│ ➥ Reverter sticker para imagem
+│ ⭐️ ${prefixo}rename (menc-sticker)
+│ ➥ Renomeia descricao do sticker
+│ 🔀 ${prefixo}figu-random ex: 5
+│ ➥ Pacotes de figurinhas
+│ 😡 ${prefixo}figu-raiva ex: 5
+│ ➥ Pacotes de figurinhas
+│ 🎨 ${prefixo}figu-desenho ex: 5
+│ ➥ Pacotes de figurinhas
+│ 😂 ${prefixo}figu-engracada ex: 5
+│ ➥ Pacotes de figurinhas
+│ 🤖 ${prefixo}figu-roblox ex: 5
+│ ➥ Pacotes de figurinhas
+│ 🌸 ${prefixo}figu-coreana ex: 5
+│ ➥ Pacotes de figurinhas
+│ 🐾 ${prefixo}figu-animais ex: 5
+│ ➥ Pacotes de figurinhas
+│ 💫 ${prefixo}figu-anime ex: 5
+│ ➥ Pacotes de figurinhas
+│ 😜 ${prefixo}figu-emojis ex: 5
+│ ➥ Pacotes de figurinhas
+│ 🧼 ${prefixo}figu-flork ex: 5
+│ ➥ Pacotes de figurinhas
 ╰─────────────⟡
 
-╭─⟢ 𝘼𝙇𝙀𝘼𝙏𝙊𝙍𝙄𝙊𝙎
-│🧠 ${prefixo}gpt (pergunta)
-│🎭 ${prefixo}plaq 1-5 
-│🎭 ${prefixo}igstalk (nome)
-│🎭 ${prefixo}tiktokstalk (nome)
-│❤️ ${prefixo}waifu
-│❤️ ${prefixo}pinterest
-│❤️ ${prefixo}metadinha
-╰─────────────⟡`
+╭─⟢ 🎲 *ALEATORIOS*
+│ 🧠 ${prefixo}gpt (pergunta)
+│ ➥ Inteligencia artificial
+│ 🎨 ${prefixo}plaq 1-5 (nome)
+│ ➥ Imagem garotas de calsinha
+│ 🕵🏻 ${prefixo}igstalk (nome)
+│ ➥ Investigar perfil instagram 
+│ ❤️ ${prefixo}waifu (foto)
+│ ➥ Imagem erotica
+│ 🎨 ${prefixo}pinterest (nome)
+│ ➥ Pesquisa de imagens
+╰─────────────⟡`;
 }
 exports.menu = menu
